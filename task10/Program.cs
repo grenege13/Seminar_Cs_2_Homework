6595840b91c3fda2726a694ce255033383f99e74
Ilya Fedorov <grenege13@gmail.com>
2022-08-09 13:36:59 +0300

    Число трёхзначное - возьми с полки пирожок))

---
+++ task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// Нужно сделать через числа, без индексов строк.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int number = 0;
+while (number == 0)
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+
+    if (number / 100 < 1 || number / 100 > 10)
+    {
+        Console.WriteLine(" ERROR: Число НЕ трёхзначное !");
+        number = 0;
+    }
+}
+
+Console.WriteLine("Правильно, число " + number + " - трёхзначное");
+
+int secondDigit = (number % 100) / 10;
+
+Console.WriteLine("Вторая цифра :  " + secondDigit);
