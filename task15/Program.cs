1ce8b3e98c9251260d35318d73a067a32c97b33e
Ilya Fedorov <grenege13@gmail.com>
2022-08-15 21:19:01 +0300

    task weekdays detect done

---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер дня недели");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber >= 1 && dayNumber <= 5)
+{
+    Console.WriteLine("Этот день недели рабочий");
+}
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Этот день недели выходной");
+    }
+
+if (dayNumber < 1 || dayNumber > 7)
+{
+    Console.WriteLine("Дня недели с таким порядковым номером не существует.");
+}
